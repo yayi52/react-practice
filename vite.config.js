e9ab382d63f74c517e9851d2ea683a14b0568852
yayi52 <mark210502@gmail.com>
2026-03-12 02:56:52 +0800

    feat:部屬變更

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //開發中、產品路徑
+  base: process.env.Node_ENV === 'production'? '/react-practice/' : '/',
   plugins: [react()],
   server: {
     proxy: {
